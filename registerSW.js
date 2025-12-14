3b30f0e012aaafb6e65dad3400876c8a2e0816ff
mohamedsugule-alt <mohamedsugule@gmail.com>
2025-12-14 12:18:18 +0300

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ifiye-book-club/sw.js', { scope: '/ifiye-book-club/' })})}
